0fe103ae0bb8f4dea54188c7a2073c9a386e8a69
Koki Sakai <likuor0528@gmail.com>
2022-06-15 15:38:45 -0700

    Done ex

--- week4/day3/cornercoin/index.js
+++ week4/day3/cornercoin/index.js
@@ -1,24 +1,67 @@
-let balance = 500.0;
+// let balance = 500.0;
+class Account {
+  constructor(username, balance) {
+    this.username = username;
+    this.balance = balance;
+    this.history = 0;
+  }
+}
 
-class Withdrawal {
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      this.account.balance += this.value;
+      this.account.history += this.value;
+    } else {
+      console.log("+++++++++It's not allowed to withdrow the money+++++++++");
+    }
+  }
+
+  isAllowed() {
+    let cal = this.account.balance + this.value;
+    if (cal < 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
   }
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+console.log('=================================================');
+const myAccount = new Account('snow-patrol', 500.0);
+console.log('Balance:', myAccount.balance);
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
+
+console.log('=================================================');
